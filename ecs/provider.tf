54cfab09e698f0e0dc91a0a2e1c83d942f46deb4
Manojkumarmh <160715812+Manojkumarmh@users.noreply.github.com>
2024-05-28 11:09:23 +0530

    Update provider.tf

--- ecs/provider.tf
+++ ecs/provider.tf
@@ -1,8 +1,14 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
+
 provider "aws" {
-  access_key = "add-your-key"
-  secret_key = "add-your-access_key"
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
   region     = var.aws_region
-  #if you are running from AWS ec2 linux instance please use bellow credentials section
-  #shared_credentials_file = "$HOME/.aws/credentials"
-  #profile = "default"
 }
