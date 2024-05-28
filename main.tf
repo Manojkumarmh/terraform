d5c40d956bb83014a73bb4d5f379e544857717a8
Manojkumarmh <160715812+Manojkumarmh@users.noreply.github.com>
2024-05-28 12:25:13 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -98,8 +98,8 @@ resource "aws_security_group" "allow_all" {
 
 resource "aws_instance" "web-1" {
     #ami = "${data.aws_ami.my_ami.id}"
-    ami = "ami-0d857ff0f5fc4e03b"
-    availability_zone = "us-east-1a"
+    ami = "ami-0bb84b8ffd87024d8"
+    availability_zone = "us-east-1"
     instance_type = "t2.micro"
     key_name = "LaptopKey"
     subnet_id = "${aws_subnet.subnet1-public.id}"
