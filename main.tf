348f2196d9ed0afd354c088c46862d6df75581a8
Manojkumarmh <160715812+Manojkumarmh@users.noreply.github.com>
2024-05-28 12:30:36 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -101,7 +101,7 @@ resource "aws_instance" "web-1" {
     ami = "ami-0bb84b8ffd87024d8"
     availability_zone = "us-east-1a"
     instance_type = "t2.micro"
-    key_name = "LaptopKey"
+    key_name = "sample"
     subnet_id = "${aws_subnet.subnet1-public.id}"
     vpc_security_group_ids = ["${aws_security_group.allow_all.id}"]
     associate_public_ip_address = true	
