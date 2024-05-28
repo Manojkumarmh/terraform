477d6d9079c704891524cbdaafb44ffbe2bd7d1b
Manojkumarmh <160715812+Manojkumarmh@users.noreply.github.com>
2024-05-28 11:10:24 +0530

    Update variables.tf

--- ecs/variables.tf
+++ ecs/variables.tf
@@ -1,3 +1,15 @@
+variable "aws_access_key" {
+  description = "AWS access key"
+  type        = string
+  default     = ""
+}
+
+variable "aws_secret_key" {
+  description = "AWS secret key"
+  type        = string
+  default     = ""
+}
+
 variable "aws_region" {
   default     = "eu-west-1"
   description = "aws region where our resources going to create choose"
